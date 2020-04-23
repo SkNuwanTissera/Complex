faef0e36b4095fb5ab1633f92266ec59d9b7c309
SkNuwanTissera <nuwan.tissera@my.sliit.lk>
2020-04-23 11:50:17 +0530

    server app routes

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 const {Pool} = require('pg');
 const pgClient = new Pool({
     user: keys.pgUser,
-    host: keys.pgHost,
+    host: keys.pgHost,  
     database: keys.pgDatabase,
     password: keys.pgPassword,
     port: keys.pgPort
@@ -20,3 +20,52 @@ pgClient.on('error', ()=> console.log('Log PG connection'));
 
 pgClient.query('CREATE TABLE IF NOT EXISTS VALUES (number INT)')
     .catch(err => console.log(err));
+
+//Redis client setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+
+const redisPublisher = redisClient.duplicate();
+
+//Express route handlers
+app.get('/',(req,res)=>{
+    res.send("Hi");
+});
+
+app.get('/values/all', async(req,res) => {
+    const values = await pgClient.query('SELECT * from VALUES');
+    res.send(values.rows);
+});
+
+app.get('/values/all', async(req,res) => {
+    redisClient.hgetall('values', (err, values)=>{
+       res.send(values); 
+    });
+});
+
+app.post('/values', async(req,res) => {
+
+    const index = req.body.index;
+
+    if (parseInt(Index) > 40){
+        return res.status(422).send('Index too high');
+    }
+     
+    // Replace nothing yet with new fibonachi value.
+    redisClient.hset('values', index, 'Nothing yet!');
+    redisPublisher.publish('insert', index);
+
+    pgClient.query('INSERT INTO VALUES(number) VALUES($1)', [index]);
+
+    res.send({working: true});
+});
+
+app.listen(5000, err => {
+    console.log('Listening on port 5000')
+})
+
+
